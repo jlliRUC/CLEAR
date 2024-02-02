f3d3eeb573ec9ccaffd92b40dd20e0e8633a7275
jlliRUC <117210705+jlliRUC@users.noreply.github.com>
2024-02-02 12:20:57 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,11 +1,70 @@
+import torch
 from torch.nn.utils import clip_grad_norm_
 import time
 import os
 import shutil
 import logging
-from model import *
+from model.Embedding import SEmbed
+from model.CLEAR import CLEAR
+from model.ProjectionHead import ProjectionHead
 from losses import get_loss
 from dataset import ClearDataLoader
+from model.utils_model import load_pretrained
+import copy
+import torch.nn as nn
+
+
+def load_pretrain(pretrain_mode, pretrain_file, vocab_size):
+    if pretrain_mode == "np":
+        embed_c = None
+    elif pretrain_mode == "pf":
+        pretrained_cell_embeddings = load_pretrained(pretrain_file, vocab_size)
+        embed_c = nn.Embedding.from_pretrained(pretrained_cell_embeddings, freeze=True)
+        for param in embed_c.parameters():
+            param.requires_grad = False
+    elif pretrain_mode == "pt":
+        pretrained_cell_embeddings = load_pretrained(pretrain_file, vocab_size)
+        embed = nn.Embedding.from_pretrained(pretrained_cell_embeddings, freeze=False)
+        for param in embed.parameters():
+            param.requires_grad = False
+    else:
+        print("Unknown pretrain marker!")
+    return embed_c
+
+
+def get_model(model_name, vocab_size, embed_size, hidden_size, dropout, num_heads, num_layers, bidirectional, pretrain_mode="np", pretrain_file=None):
+    if model_name == "clear-RNN":
+        from model.NaiveRNN import RNNEncoder
+        embed_s = SEmbed(vocab_size, embed_size)
+        embed_c = load_pretrain(pretrain_mode, pretrain_file, vocab_size)
+        encoder = RNNEncoder(None, embed_s, embed_c, hidden_size, bidirectional, dropout, num_layers)
+        projector = ProjectionHead(embed_size, int(embed_size / 2), int(embed_size / 4), batch_norm=True)
+        model = CLEAR(encoder=encoder, projector=projector)
+        return model
+
+    elif model_name == "clear-DualRNN":
+        from model.DualRNN import DualRNNEncoder
+        embed_s = SEmbed(vocab_size, embed_size)
+        embed_c = load_pretrain(pretrain_mode, pretrain_file, vocab_size)
+        encoder = DualRNNEncoder(None, embed_s, embed_c, hidden_size, bidirectional, dropout, num_layers)
+        projector = ProjectionHead(embed_size, int(embed_size / 2), int(embed_size / 4), batch_norm=True)
+        model = CLEAR(encoder=encoder, projector=projector)
+        return model
+
+    elif model_name == "clear-Trans":
+        from model.transformer import TransEncoder, MultiHeadedAttention, PositionwiseFeedForward, PositionalEncoding, \
+            EncoderLayer
+        attn = MultiHeadedAttention(num_heads, embed_size)
+        ff = PositionwiseFeedForward(embed_size, int(embed_size * 2), dropout)
+        position = PositionalEncoding(embed_size, dropout)
+        projector = ProjectionHead(embed_size, int(embed_size / 2), int(embed_size / 4), batch_norm=True)
+        c = copy.deepcopy
+
+        encoder = TransEncoder(embed_s=SEmbed(vocab_size, embed_size),
+                               embed_p=c(position),
+                               layer=EncoderLayer(embed_size, c(attn), c(ff), dropout),
+                               num_layers=num_layers)
+        return CLEAR(encoder=encoder, projector=projector)
 
 
 def init_parameters(model):
@@ -20,7 +79,7 @@ def init_parameters(model):
 def save_checkpoint(state, is_best, args):
     torch.save(state, args.checkpoint)
     if is_best:
-        shutil.copyfile(args.checkpoint, f"data/{args.dataset_name}/{args.file_suffix}_best.pt")
+        shutil.copyfile(args.checkpoint, f"data/{args.dataset_name}/{args.suffix}_best.pt")
 
 
 def validate(args, valid_datasets, model):
@@ -50,17 +109,8 @@ def validate(args, valid_datasets, model):
     return total_loss / num_iteration
 
 
-def get_model(vocab_size, embed_size, hidden_size, dropout, num_layers, bidirectional, model_name):
-    if model_name == 'clear-S':
-        embed = SEmbed(vocab_size, embed_size)
-        encoder = SEncoder(None, embed, hidden_size, bidirectional, dropout, num_layers)
-        projector = ProjectionHead(embed_size, int(embed_size / 2), int(embed_size / 4), batch_norm=True)
-
-        return SClear(encoder, projector)
-
-
 def train(args):
-    logging.basicConfig(filename=f"data/{args.dataset_name}/{args.file_suffix}_training.log", filemode='a',
+    logging.basicConfig(filename=f"data/{args.dataset_name}/{args.suffix}_training.log", filemode='a',
                         level=logging.INFO)
 
     train_datasets = ClearDataLoader(dataset_name=args.dataset_name,
@@ -69,38 +119,46 @@ def train(args):
                                      transformer_list=args.transformer_list,
                                      istrain=True,
                                      batch_size=args.batch_size,
-                                     spatial_type=args.spatial_type,
+                                     partition_method=args.partition_method,
                                      cell_size=args.cell_size,
                                      minfreq=args.minfreq,
-                                     density=args.density,
                                      shuffle=args.shuffle)
     print("Loading train datasets...")
     train_datasets.load()
     print(f"Train dataset size: {train_datasets.num_s}")
-    
+
     valid_datasets = ClearDataLoader(dataset_name=args.dataset_name,
                                      num_train=args.num_train,
                                      num_val=args.num_val,
                                      transformer_list=args.transformer_list,
                                      istrain=False,
                                      batch_size=args.batch_size,
-                                     spatial_type=args.spatial_type,
+                                     partition_method=args.partition_method,
                                      cell_size=args.cell_size,
                                      minfreq=args.minfreq,
-                                     density=args.density,
                                      shuffle=args.shuffle)
     print("Loading valid data...")
     valid_datasets.load()
     print(f"Valid dataset size: {valid_datasets.num_s}")
 
     # model
-    model = get_model(vocab_size=args.vocab_size,
+    if args.pretrain_mode == "np":
+        print("Train without pretraining")
+        pretrain_file = None
+    else:
+        pretrain_file = f"data/{args.dataset_name}/{args.dataset_name}_size-{args.hidden_size}_cellsize-{args.cell_size}_minfreq-{args.minfreq}_node2vec.pkl"
+
+    model = get_model(model_name=args.model_name,
+                      vocab_size=args.vocab_size,
                       embed_size=args.embed_size,
                       hidden_size=args.hidden_size,
                       dropout=args.dropout,
+                      num_heads=args.num_heads,
                       num_layers=args.num_layers,
                       bidirectional=args.bidirectional,
-                      model_name=args.model_name)
+                      pretrain_mode=args.pretrain_mode,
+                      pretrain_file=pretrain_file
+                      )
 
     print(f"model {model}")
 
@@ -113,6 +171,7 @@ def train(args):
 
     # optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_degrade_step, gamma=args.lr_degrade_gamma)
 
     # load model state and optmizer state
     if os.path.isfile(args.checkpoint):
@@ -149,7 +208,7 @@ def train(args):
             optimizer.zero_grad()
             # batch dataset
             s_list, lengths_list = train_datasets.get_one_batch()
-            #print(f"Iteration {start_iteration}: max length is {[max(i) for i in lengths_list]}")
+            # print(f"Iteration {start_iteration}: max length is {[max(i) for i in lengths_list]}")
             if s_list is None:
                 print(f"Current epoch finished. ")
                 break
@@ -160,7 +219,7 @@ def train(args):
             output_list = [model(s, lengths=lengths) for s, lengths in zip(s_list, lengths_list)]
             h_list, z_list = [output[0] for output in output_list], [output[1] for output in output_list]
             features = torch.cat(z_list, dim=0).to(args.device)
-            
+
             # InfoNCE Loss
             criteration = get_loss(args.loss)
             loss = criteration(args.batch_size, args.n_views, args.temperature, features, args.device)
@@ -201,6 +260,8 @@ def train(args):
                                  "optimizer": optimizer.state_dict()
                                  }, is_best, args)
             start_iteration += 1
+
+        scheduler.step()
         print(f"Epoch {current_epoch} costs: {time.time() - epoch_start:.4f} s")
         epoch_start = time.time()
     print(f"Training costs: {time.time() - start:.4f}s")
